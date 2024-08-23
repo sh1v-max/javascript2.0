71b1f19aa601a18e5f3667befdea10e830d0e414
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-08-23 16:56:09 +0000

    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,3 +2,24 @@
 
 //alert( 3 + 3) //do same on ispect chrome and see the output
 //aler require different syntex..... we are using node js not browser
+
+console.log(3+
+    3) //code readability should be high
+
+console.log("shiv")
+
+let name="hitesh"
+let age= 18
+let isLoggedIn = false
+
+// number =>2 to power 53
+// bigint
+// string => true/false
+// null = standalone value
+// undefined => 
+// symbol => unique
+// object
+
+console.log(typeof age);
+console.log(typeof null)
+console.log(typeof undefined)
