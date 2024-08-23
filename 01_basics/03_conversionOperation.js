2c89421d1a36cfe4408d3393f1999bd4369c10b2
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-08-23 18:46:58 +0000

    some changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -36,3 +36,35 @@ let stringNumber=String(someNumber)
 console.log(stringNumber);
 console.log(typeof(stringNumber));
 
+//**********************operations**************** */
+
+let value = 3
+let negValue=-value
+console.log(negValue);
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+
+let str1 = "hello"
+let str2=" shiv"
+
+let str3=str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+// see outputs carefully 
+console.log(+true);
+console.log(+"");
+
+
+
+
+
+
+
+
