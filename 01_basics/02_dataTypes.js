c0a2d892ed9a652b65ab6faa622a331573051f3c
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-08-24 06:47:28 +0000

    day 2

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,3 +1,13 @@
+/*
+1. primitive data type ... call by value
+original value doesnt change, you will be provided with 
+copy of data every type you call it rather than the
+original data... thats why called call by value
+
+7 types: string, number, Boolean, null, undefined, symbol, Bigint
+
+*/
+
 "use strict"; //treat all js code as newer version
 
 //alert( 3 + 3) //do same on ispect chrome and see the output
@@ -10,7 +20,7 @@ console.log("shiv")
 
 let name="hitesh"
 let age= 18
-let isLoggedIn = false
+//let isLoggedIn = false
 
 // number =>2 to power 53
 // bigint
@@ -23,3 +33,66 @@ let isLoggedIn = false
 console.log(typeof age);
 console.log(typeof null)
 console.log(typeof undefined)
+
+const score=10
+const scoreValue= 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userAge;             //undefined
+let userAge2=undefined   //undefined
+
+//to declare symbol
+const id= Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===Symbol);
+
+// const bigNumber=32425345353253453n //add n at end
+
+
+
+
+/*
+2. refrence(non premetive)
+
+types: arrays, object, function
+
+dynamically typed language or statically typed language...
+const score=false
+const score =100
+const score ="false"
+we dont initialise data type, javascript handles it iteself 
+accordingly so its dynamically typed language or week typing
+*/
+
+const heros = ["shaktiman", "nagraj", "doga"]; // array
+let myObj={                                    // object
+    name:"shiv",    // initialise in curly brackets
+    age:22,         // data type can be any thing       
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+    
+}
+
+console.log(typeof bigNumber);     // undefined
+console.log(typeof outsideTemp);   // object
+console.log((typeof myFunction));  // function
+
+/*
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+*/
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,27 @@
+// console.log(2>1);
+// console.log(2<1);
+
+
+//not lets compare diff datatypes
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0); //false
+console.log(null==0); //false
+console.log(null>=0); //true
+/*
+the reason is that an equality check (==) and comparision
+(<>>=<=) work differently,
+comparision convert null to a number, treat it as 0.
+thats why (null>=0); //true and (null>0); //false
+*/
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+// here it will be false for all the cases
+
+//(===) string check, it also checks the data type
+console.log("2"==2);//true
+console.log("2"===2); //false
+
