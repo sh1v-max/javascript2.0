c914b8ee16cf1ac93a7df9fc3b30091ceaf7d459
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2024-08-24 10:26:01 +0000

    day 2

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,69 @@
+const name = "shiv"
+const repoCount = 50
+
+//another way to declare string
+const gameName = new String('shiv-gc')
+/*
+first method only define variable and store sting
+while second method store its information also
+==see refrence image
+
+now you can easily perform operations on 
+'gameName'
+*/
+console.log(gameName); //it wont pring string name
+// see output  [String: 'shivgc']
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("t")); //-1
+console.log(gameName.indexOf("v")); //3
+
+const newString=gameName.substring(0,2) // here you can only give +ve arg
+console.log(newString);
+
+const anotherString = gameName.slice(-5,5) //here you can give -ve arg also
+console.log(anotherString);
+
+const newStringOne="    shiv    "
+console.log(newStringOne);
+console.log(newStringOne.trim());  //removes backspace
+
+const url="https://shiv.com/shiv%20shingh"
+// u want to replace 
+console.log(url.replace('%20','_'));
+console.log(url.includes('shiv'));
+// to check if paramtr is presend or not
+console.log(gameName.split('-'));
+
+
+console.log(name + repoCount + "value");
+/*
+we are normally concatinating using ++
+this line doesnt look good in prduct buildup perspective
+ie its not much readable and to make it more readable, we will use
+string interpolation.
+and from now on we will always use same method
+*/
+
+console.log(`hello my name is ${gameName} and my repo count is ${repoCount}`);
+
+/*
+here we are using backtick(`) and placeholders(using ${}) while 
+writing desired content alongside
+this look more readable and industry friendly
+*/
+
+
+/*-------OUTPUT
+[String: 'shiv-gc']
+s
+{}
+7
+SHIVGC
+shiv50value
+hello my name is shivgc and my repo count is 50
+*/
